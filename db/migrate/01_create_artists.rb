d2e73ce5f3231d8ce0cf8d6794ea503b873d089e
Flatiron School <leslie.hsu@flatironschool.com>
2017-12-04 19:04:56 -0500

    Done.

---
+++ db/migrate/01_create_artists.rb
@@ -0,0 +1,14 @@
+
+
+#so, this is a migration class, and includes "do" and "undo" types of methods
+
+class CreateArtists < ActiveRecord::Migration[4.2]
+    def change
+      create_table :artists do |t|
+        t.string :name
+        t.string :genre
+        t.integer :age
+        t.string :hometown
+      end
+    end
+  end
